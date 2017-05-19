41c163a8706919ea884f7c28edce4580222e5c22
byron <wangxue_li2108@126.com>
2017-05-19 22:58:07 +0800

    add test uint package

---
+++ src/test/java/com/cbm/unit/TestTemplate.java
@@ -0,0 +1,20 @@
+/**
+ * @Title: TestTemplate.java
+ * @Package com.cbm.unit
+ * @Description: TODO(用一句话描述该文件做什么)
+ * @author byron
+ * @date 2017年5月19日
+ * @version V1.0
+ */
+package com.cbm.unit;
+
+/**
+ * @ClassName: TestTemplate
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ * @author byron
+ * @date 2017年5月19日
+ *
+ */
+public class TestTemplate {
+
+}
